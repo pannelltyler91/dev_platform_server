df127f79affdf8275087bd9c0cf05dd1b2aaee18
unknown <you@example.com>
2021-11-02 20:52:19 -0400

    first commit

---
+++ migrations/20211103003953-create-users.cjs
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      github: {
+        type: Sequelize.STRING
+      },
+      linkedin: {
+        type: Sequelize.STRING
+      },
+      portfolio: {
+        type: Sequelize.STRING
+      },
+      currentLanguages: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+      },
+      newLanguages: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import bcrypt from 'bcrypt'
+import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 3001
+
+app.use(cors());
 app.use(express.json())
 app.use(
     express.urlencoded({
       extended: true,
     })
   );
+
+  app.get('/', (req,res) =>{
+    res.send('I love coding!')
+  })
   
 
 
