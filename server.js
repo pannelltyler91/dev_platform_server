36143985a420c66b31a4c5941caa6276786ac6fa
unknown <you@example.com>
2021-11-13 10:52:07 -0500

    got rid of middleware i wasnt using

--- server.js
+++ server.js
@@ -9,21 +9,7 @@ const jwt = require("jsonwebtoken");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split("")[1];
-  if (token == null) {
-    return res.status(401);
-  } else {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user));
-  }
-  if (err) {
-    res.status(403);
-  } else {
-    req.user = user;
-    next();
-  }
-};
+
 app.use(cors());
 app.use(express.json());
 app.use(
